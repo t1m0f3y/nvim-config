c2c30432bd24e2ab51cf63fbce226b5ba087f32d
t1m0f3y <t.leonenko2001@gmail.com>
2024-08-15 05:45:44 +0700

    add a map for opening a URL using browser

--- lua/my/remap.lua
+++ lua/my/remap.lua
@@ -3,3 +3,11 @@ vim.g.mapleader = " "
 local map = vim.api.nvim_set_keymap
 map('n', '<C-d>', '<C-d>zz', {noremap = true, silent = false})
 map('n', '<C-u>', '<C-u>zz', {noremap = true, silent = false})
+--- map('n', 'gx', ':execute "!xdg-open " . shellescape("<cWORD>")<CR>', 
+	--- {noremap = true, silent = false})
+
+--- add a gX normal shortcut for opening a URL in browser "browserName"
+local browserName = "firefox"
+local gX_string = string.format(':silent execute "!%s " . shellescape("<cWORD>")<CR>', 
+	browserName)
+map('n', 'gX', gX_string, {noremap = true, silent = false})
